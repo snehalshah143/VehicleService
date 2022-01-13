f3ed6f44d3deb53bdbd588c27e09f87a41541a2c
shaht_000 <snehal.shah143@gmail.com>
2022-01-13 10:19:24 +0530

    All the API tested

--- src/main/java/com/snehal/carservice/model/TimeSlot.java
+++ src/main/java/com/snehal/carservice/model/TimeSlot.java
@@ -1,6 +1,8 @@
 package com.snehal.carservice.model;
 
-public enum TimeSlot {
+import java.io.Serializable;
+
+public enum TimeSlot implements Serializable{
 	PEAK_TIME(1,"6.30 AM - 9.30 AM ",50),
 	NORMAL_TIME(2,"9.30AM - 12 PM   AND 5.30PM - 8.30PM",0),
 	FREE_TIME(3,"12PM - 5.30 PM ANd NIGHT TIME AFTER 8.30PM",-50);
